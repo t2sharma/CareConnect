e1847cdf534285650ed38e00aa0bd482da2c990b
Dhananjai Sharma Talipadi Ravindra (dtalipad) <dtalipad@cisco.com>
2025-12-10 00:05:21 -0800

    Direct App launch

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { LandingPage } from './components/landing/LandingPage';
 import { MainLayout } from './layouts/MainLayout';
 
 const App: React.FC = () => {
-  const [showDemo, setShowDemo] = React.useState(false);
-
-  const handleBackToLanding = React.useCallback(() => {
-    setShowDemo(false);
-  }, []);
-
-  if (showDemo) {
-    return <MainLayout onBackToLanding={handleBackToLanding} />;
-  }
-
-  return <LandingPage onNavigateToDemo={() => setShowDemo(true)} />;
+  return <MainLayout />;
 };
 
 export default App;
